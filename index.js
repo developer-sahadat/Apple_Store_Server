70f612d618a9966ad23a014f19750f4f741deb89
developer-sahadat <mdridoysikder862@gmail.com>
2022-05-02 05:28:46 +0600

    verify Json Web Token

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+var jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// verify Json Web Token :
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ massage: "Sorry Unauthorized" });
+  }
+
+  jwt.verify(
+    authHeader,
+    process.env.ACCESS_TOKEN_SECRET,
+    function (err, decoded) {
+      if (err) {
+        return res
+          .status(403)
+          .send({ massage: " Forbidden, does not have access " });
+      }
+
+      req.decoded = decoded;
+      next();
+    }
+  );
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.czcbx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -21,6 +45,15 @@ async function run() {
     await client.connect();
     const inventoryCollection = client.db("phonesy").collection("inventory");
     const supplierCollection = client.db("phonesy").collection("supplier");
+
+    /*___________Access token secrets Auth code start here_____________*/
+    app.post("/login", (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "2d",
+      });
+      res.send({ accessToken });
+    });
 
     /*___________All data load API code start here_____________*/
     app.get("/inventory", async (req, res) => {
@@ -46,13 +79,19 @@ async function run() {
     });
 
     /*___________ My inventory items get API code start here_____________*/
-    app.get("/my-items", async (req, res) => {
-      const email = req.query.email;
-      const query = { email };
-      console.log(query);
-      const cursor = inventoryCollection.find(query);
-      const myItems = await cursor.toArray();
-      res.send(myItems);
+    app.get("/my-items", verifyJWT, async (req, res) => {
+      const decodedEmail = req?.decoded?.email;
+      const email = req?.query?.email;
+
+      if (email === decodedEmail) {
+        const query = { email };
+        const cursor = inventoryCollection.find(query);
+        const myItems = await cursor.toArray();
+
+        res.send(myItems);
+      } else {
+        res.status(403).send({ massage: " Forbidden, does not have access " });
+      }
     });
     /*___________ My inventory items delete  API code start here_____________*/
     app.delete("/my-items/:id", async (req, res) => {
